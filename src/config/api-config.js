5b3d5fe2e7ead7cc5bf730de89fb98e44b8ffa4d
ayaan <seconddate@naver.com>
2023-06-05 18:47:28 +0900

    API 주소변경

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,2 +1,2 @@
 //export const DEV_API_BASE_URL = 'https://mtc-openai-api-dev.azurewebsites.net/';
-export const API_BASE_URL = 'https://mtc-openai.azurewebsites.net';
+export const API_BASE_URL = 'https://app-service-hs-workshop.azurewebsites.net/';
